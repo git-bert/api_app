51c68a057fc428a81452b3d55af5607bb94dc1a9
git-bert <53254155+git-bert@users.noreply.github.com>
2024-10-10 07:59:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-echo "Hello, world!";
+echo "Connected!";
+
+// Capture query parameters from the URL
+$access_token = isset($_GET['access_token']) ? $_GET['access_token'] : null;
+
+// Check if the 'Access Token' parameter is present
+if ($code) {
+    echo "<h1>Authorization Code Received:</h1>";
+    echo "<p>Access Token: " . htmlspecialchars($access_token) . "</p>";
+} else {
+    echo "<h1>Error: Access Token Received</h1>";
+}
+
 ?>
